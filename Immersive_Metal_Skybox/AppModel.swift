eb8b1cfbb0ffbc6e5830547748b950d05ad799d0
RandomYang <decoderyang@gmail.com>
2025-01-24 00:57:20 +0800

    Initial Commit

---
+++ Immersive_Metal_Skybox/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Immersive_Metal_Skybox
+//
+//  Created by randomyang on 2025/1/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Immersive_Metal_SkyboxTests/Immersive_Metal_SkyboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  Immersive_Metal_SkyboxTests.swift
+//  Immersive_Metal_SkyboxTests
+//
+//  Created by randomyang on 2025/1/24.
+//
+
+import Testing
+@testable import Immersive_Metal_Skybox
+
+struct Immersive_Metal_SkyboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
